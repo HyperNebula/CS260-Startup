3ae01bbdaa393ecebd6d33177c0c003ebf30bf11
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-02-10 15:59:14 -0700

    Added header and footer to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -9,5 +9,33 @@ import { BrowserRouter, NavLink, Route, Routes, useLocation } from 'react-router
 //import { Settings } from './settings/settings';
 
 export default function App() {
-  return (<div className="body bg-dark text-light">App will display here</div>);
+  return (
+    <div className="body">
+        <header className="nav-bar">
+                <div className="logo">
+                    <img src="/check-mark.png" alt="Library Logo" style={{ width: "1.5em", height: "auto" }} />
+                    <span>CheckOff</span>
+                </div>
+
+                <nav className="nav-links">
+                    <a href="index.html">Log Out</a> |
+                    <a href="library.html">Library</a>  |
+                    <a href="feed.html">Feed</a>
+                </nav>
+
+                <div className="account-settings">
+                    <a href="settings.html">Account Settings</a>
+                </div>
+        </header>
+
+        <main>Body Here</main>
+
+        <footer className="credit">
+            <span>David Deskins | </span>
+            <a href="https://github.com/HyperNebula/CS260-Startup/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>Github Repository</a>
+        </footer>
+
+    </div>
+    
+  );
 }
