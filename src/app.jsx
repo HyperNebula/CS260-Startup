7eae2ea7a298706892c5b66ed6e5412a69665c12
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-02-10 15:46:12 -0700

    Added app.jsx and app.css

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+import { BrowserRouter, NavLink, Route, Routes, useLocation } from 'react-router-dom';
+//import { Login } from './login/login';
+//import { Library } from './library/library';
+//import { Feed } from './feed/feed';
+//import { Settings } from './settings/settings';
+
+export default function App() {
+  return (<div className="body bg-dark text-light">App will display here</div>);
+}
