87e18d36d49b83c1046c907c47a3609a494ce70c
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-02-11 16:06:05 -0700

    Added login.jsx body

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 
 export function Login() {
-  return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
-    </main>
-  );
+  	return (
+		<main className='login-page'>
+
+			<h1 id="login-title">CheckOff</h1>
+
+			<div className="login_box">
+
+				<h2 style={{ marginBottom: 30 }}>Login</h2>
+
+				<form action="library.html">
+					<div className="input-group">
+						<span className="icon">👤</span>
+						<input type="text" name="username" placeholder="Username" minlength="3" required/>
+					</div>
+					
+					<div className="input-group">
+						<span className="icon">🗝️</span>
+						<input type="password" name="password" placeholder="Password" minlength="8" required/>
+					</div>
+					
+					<br/>
+
+					<div className="form-actions">
+						<input type="submit" value="Login"/>
+						<input type="submit" value="Create Account" formaction="settings.html"/>
+					</div>
+				</form>
+			
+			</div>
+
+		</main>
+  	);
 }
