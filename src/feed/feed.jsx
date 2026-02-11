9147c0ce802e923f7156a0ba06ecd4ea02b7c98f
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-02-11 12:53:04 -0700

    added feed body

--- src/feed/feed.jsx
+++ src/feed/feed.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../app.css';
+
 
 export function Feed() {
   return (
@@ -8,72 +8,72 @@ export function Feed() {
 
         <div className="activity-feed">
 
-        <article>
-            <header>
-                <img src="/placeholder_user_profile_image.png" alt="Profile Pic" width="30" height="30"/>
-                <span><strong>Sarah</strong> rated <strong>Lord of the Rings</strong> <time>2 hours ago</time></span>
-            </header>
-            
-            <blockquote>
-                "MOVIE REVIEW"
-            </blockquote>
-            
-            <p>Rating: ⭐⭐⭐⭐⭐</p>
-        </article>
+            <article>
+                <header>
+                    <img src="/placeholder_user_profile_image.png" alt="Profile Pic" width="30" height="30"/>
+                    <span><strong>Sarah</strong> rated <strong>Lord of the Rings</strong> <time>2 hours ago</time></span>
+                </header>
+                
+                <blockquote>
+                    "MOVIE REVIEW"
+                </blockquote>
+                
+                <p>Rating: ⭐⭐⭐⭐⭐</p>
+            </article>
 
-        <article>
-            <header>
-                <img src="/placeholder_user_profile_image.png" alt="Profile Pic" width="30" height="30"/>
-                <span><strong>Mike</strong> just watched <strong>A Good Movie</strong>
-                <time>4 hours ago</time></span>
-            </header>
-            
-            <figure>
-                <img src="/placeholder_movie_poster.png" alt="Movie Poster" width="100"/>
-            </figure>
-        </article>
+            <article>
+                <header>
+                    <img src="/placeholder_user_profile_image.png" alt="Profile Pic" width="30" height="30"/>
+                    <span><strong>Mike</strong> just watched <strong>A Good Movie</strong>
+                    <time>4 hours ago</time></span>
+                </header>
+                
+                <figure>
+                    <img src="/placeholder_movie_poster.png" alt="Movie Poster" width="100"/>
+                </figure>
+            </article>
 
-        <article>
-            <header>
-                <img src="/placeholder_user_profile_image.png" alt="Profile Pic" width="30" height="30"/>
-                <span><strong>Jessica</strong> added 3 movies to their <em>"Horror Night"</em> list
-                <time>yesterday</time></span>
-            </header>
+            <article>
+                <header>
+                    <img src="/placeholder_user_profile_image.png" alt="Profile Pic" width="30" height="30"/>
+                    <span><strong>Jessica</strong> added 3 movies to their <em>"Horror Night"</em> list
+                    <time>yesterday</time></span>
+                </header>
 
-            <ul>
-                <li>Horror Movie 1</li>
-                <li>Horror Movie 2</li>
-                <li>Horror Movie 3</li>
-            </ul>
-        </article>
+                <ul>
+                    <li>Horror Movie 1</li>
+                    <li>Horror Movie 2</li>
+                    <li>Horror Movie 3</li>
+                </ul>
+            </article>
 
-        <article>
-            <header>
-                <img src="/placeholder_user_profile_image.png" alt="Profile Pic" width="30" height="30"/>
-                <span><strong>Bob</strong> added 5 movies to their <em>"Want to watch"</em> list
-                <time>2 days ago</time></span>
-            </header>
+            <article>
+                <header>
+                    <img src="/placeholder_user_profile_image.png" alt="Profile Pic" width="30" height="30"/>
+                    <span><strong>Bob</strong> added 5 movies to their <em>"Want to watch"</em> list
+                    <time>2 days ago</time></span>
+                </header>
 
-            <ul>
-                <li>Movie 1</li>
-                <li>Movie 2</li>
-                <li>Movie 3</li>
-                <li>Movie 4</li>
-                <li>Movie 5</li>
-            </ul>
-        </article>
+                <ul>
+                    <li>Movie 1</li>
+                    <li>Movie 2</li>
+                    <li>Movie 3</li>
+                    <li>Movie 4</li>
+                    <li>Movie 5</li>
+                </ul>
+            </article>
 
-        <article>
-            <header>
-                <img src="/placeholder_user_profile_image.png" alt="Profile Pic" width="30" height="30"/>
-                <span><strong>David</strong> watched <strong>Cool Movie</strong>
-                <time>4 days ago</time></span>
-            </header>
-            
-            <figure>
-                <img src="/placeholder_movie_poster.png" alt="Movie Poster" width="100"/>
-            </figure>
-        </article>
+            <article>
+                <header>
+                    <img src="/placeholder_user_profile_image.png" alt="Profile Pic" width="30" height="30"/>
+                    <span><strong>David</strong> watched <strong>Cool Movie</strong>
+                    <time>4 days ago</time></span>
+                </header>
+                
+                <figure>
+                    <img src="/placeholder_movie_poster.png" alt="Movie Poster" width="100"/>
+                </figure>
+            </article>
 
 
         </div>
