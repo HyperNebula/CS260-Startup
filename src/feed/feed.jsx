341f538e57075a6cb2e650175cfe9662fb23cf25
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-02-24 16:57:56 -0700

    simulated feed updating

--- src/feed/feed.jsx
+++ src/feed/feed.jsx
@@ -1,83 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+class FeedUpdate {
+    constructor(updateType, username, userProfilePic, movie, updateTime, updateData) {
+        this.updateType = updateType;
+        this.username = username;
+        this.userProfilePic = userProfilePic;
+        this.movie = movie;
+        this.updateTime = updateTime;
+        this.updateData = updateData;
+    }
+};
+
+function RatingUpdate( {update} ) {
+    return (
+        <article>
+            <header>
+                <img src={update.userProfilePic} alt="Profile Pic" width="30" height="30"/>
+                <span><strong>{update.username}</strong> rated <strong>{update.movie}</strong> on <time>{update.updateTime}</time></span>
+            </header>
+
+            <p>Rating: {update.updateData}</p>
+        </article>
+    )
+}
+
+function WatchedUpdate( {update} ) {
+    return (
+        <article>
+            <header>
+                <img src={update.userProfilePic} alt="Profile Pic" width="30" height="30"/>
+                <span><strong>{update.username}</strong> just watched <strong>{update.movie}</strong> on <time>{update.updateTime}</time></span>
+            </header>
+
+            <figure>
+                <img src={update.updateData} alt={update.movie} width="100"/>
+            </figure>
+        </article>
+    )
+}
 
 export function Feed() {
-  return (
-    <main>
-        <h1 style={{ textAlign: "center" }}>Activity</h1>
 
-        <div className="activity-feed">
+    const [updates, setUpdates] = useState([new FeedUpdate("Rating", "Test User", "/placeholder_user_profile_image.png", "Movie 1", "Monday", "⭐⭐⭐⭐⭐"), new FeedUpdate("Watched", "Test User 2", "/placeholder_user_profile_image.png", "Movie 2", "Saturady", "/placeholder_movie_poster.png")]);
 
-            <article>
-                <header>
-                    <img src="/placeholder_user_profile_image.png" alt="Profile Pic" width="30" height="30"/>
-                    <span><strong>Sarah</strong> rated <strong>Lord of the Rings</strong> <time>2 hours ago</time></span>
-                </header>
-                
-                <blockquote>
-                    "MOVIE REVIEW"
-                </blockquote>
-                
-                <p>Rating: ⭐⭐⭐⭐⭐</p>
-            </article>
+    useEffect(() => {
+        setInterval(() => {
+            const newItem = new FeedUpdate("Rating", "Test User", "/placeholder_user_profile_image.png", "Movie 1", "Monday", "⭐⭐⭐⭐⭐");
+            
+            setUpdates(prevUpdates => [...prevUpdates, newItem]);
+        }, 5000);
 
-            <article>
-                <header>
-                    <img src="/placeholder_user_profile_image.png" alt="Profile Pic" width="30" height="30"/>
-                    <span><strong>Mike</strong> just watched <strong>A Good Movie</strong> 
-                    <time>4 hours ago</time></span>
-                </header>
-                
-                <figure>
-                    <img src="/placeholder_movie_poster.png" alt="Movie Poster" width="100"/>
-                </figure>
-            </article>
+        return;
+    }, []);
 
-            <article>
-                <header>
-                    <img src="/placeholder_user_profile_image.png" alt="Profile Pic" width="30" height="30"/>
-                    <span><strong>Jessica</strong> added 3 movies to their <em>"Horror Night"</em> list 
-                    <time>yesterday</time></span>
-                </header>
+   
 
-                <ul>
-                    <li>Horror Movie 1</li>
-                    <li>Horror Movie 2</li>
-                    <li>Horror Movie 3</li>
-                </ul>
-            </article>
+    return (
+        <main>
+            <h1 style={{ textAlign: "center" }}>Activity</h1>
 
-            <article>
-                <header>
-                    <img src="/placeholder_user_profile_image.png" alt="Profile Pic" width="30" height="30"/>
-                    <span><strong>Bob</strong> added 5 movies to their <em>"Want to watch"</em> list 
-                    <time>2 days ago</time></span>
-                </header>
+            <div className="activity-feed">
 
-                <ul>
-                    <li>Movie 1</li>
-                    <li>Movie 2</li>
-                    <li>Movie 3</li>
-                    <li>Movie 4</li>
-                    <li>Movie 5</li>
-                </ul>
-            </article>
+                {[...updates].reverse().map((update, index) => {
+                    if (update.updateType === "Rating") {
+                        return <RatingUpdate key={index} update={update} />;
+                    } else if (update.updateType === "Watched") {
+                        return <WatchedUpdate key={index} update={update} />;
+                    }
+        
+                    return null;
+                })}
 
-            <article>
-                <header>
-                    <img src="/placeholder_user_profile_image.png" alt="Profile Pic" width="30" height="30"/>
-                    <span><strong>David</strong> watched <strong>Cool Movie</strong> 
-                    <time>4 days ago</time></span>
-                </header>
-                
-                <figure>
-                    <img src="/placeholder_movie_poster.png" alt="Movie Poster" width="100"/>
-                </figure>
-            </article>
+            </div>
 
-
-        </div>
-
-    </main>
-  );
+        </main>
+    );
 }
