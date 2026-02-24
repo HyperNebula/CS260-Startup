8f939c35a75bba7ed93b8f09c22f56e49f0561a7
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-02-23 22:13:33 -0700

    started adding basic login javascript

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,6 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { UserData } from '../app';
 
 export function Login() {
+	
+	const navigate = useNavigate();
+
+	const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+	const [userDataSet, setUserDataSet] = useState([]);
+
+	const handleLogin = (e) => {
+		e.preventDefault();
+
+		setUserDataSet(JSON.parse(localStorage.getItem("userDataSet")))
+
+		for (const user of userDataSet) {
+			if (user.username == username) {
+				localStorage.setItem('userName', username);
+
+				navigate('/library');
+			} else {
+				console.log("Error, not registered");
+			}
+		}
+	};
+
+	const handleAccountCreation = (e) => {
+		e.preventDefault();
+
+		navigate('/settings');
+	}
+
   	return (
 		<main className='login-page'>
 
@@ -10,22 +42,22 @@ export function Login() {
 
 				<h2 style={{ marginBottom: 30 }}>Login</h2>
 
-				<form action="library" method='PUT'>
+				<form onSubmit={ handleLogin }>
 					<div className="input-group">
 						<span className="icon">👤</span>
-						<input type="text" name="username" placeholder="Username" minlength="3" required/>
+						<input type="text" name="username" placeholder="Username" minLength="3" required value={username} onChange={(e) => setUsername(e.target.value)}/>
 					</div>
 					
 					<div className="input-group">
 						<span className="icon">🗝️</span>
-						<input type="password" name="password" placeholder="Password" minlength="8" required/>
+						<input type="password" name="password" placeholder="Password" minLength="8" required value={password} onChange={(e) => setPassword(e.target.value)}/>
 					</div>
 					
 					<br/>
 
 					<div className="form-actions">
 						<input type="submit" value="Login"/>
-						<input type="submit" value="Create Account" formaction="settings"/>
+						<input type="button" value="Create Account" onClick={handleAccountCreation}/>
 					</div>
 				</form>
 			
