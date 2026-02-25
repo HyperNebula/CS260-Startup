799d42e9ee8403a564b1acef5dff4a1528099511
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-02-24 17:26:29 -0700

    Readded profile pic option to account settings

--- src/settings/settings.jsx
+++ src/settings/settings.jsx
@@ -64,15 +64,17 @@ export function Settings() {
                         <input type="text" id="targetUsername" value={username} onChange={(e) => setUsername(e.target.value)}/>
                     </p>
 
-{/*                    <p>
+                    <p>
                         <label>Profile Picture:</label><br/>
                         <input type="file" accept="image/png, image/jpeg"/>
                     </p>
-*/}
+
+{/*
                     <p>
                         <label>Bio:</label><br/>
                         <textarea rows="4" defaultValue={"Information on user..."}></textarea>
                     </p>
+*/}
                 </fieldset>
                 
                 <br/>
