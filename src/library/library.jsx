7cc39fb07d49b4d08e9aec5e967588434c603d5f
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-02-25 15:54:42 -0700

    Added rendering movies from DB

--- src/library/library.jsx
+++ src/library/library.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 class MovieData {
-    constructor(name, posterLink, genres, year, status) {
+    constructor(name, movieID, posterLink, genres, year, status) {
         this.name = name;
+        this.moveID = movieID;
         this.posterLink = posterLink;
         this.genres = genres;
         this.year = year;
@@ -10,7 +11,10 @@ class MovieData {
     }
 };
 
-function displayMovie( { movie }) {
+const testArray = [new MovieData("Test Movie", "1", "/placeholder_movie_poster.png", "Comedy", "2023", "To Watch"), new MovieData("Another Movie", "2", "/placeholder_movie_poster.png", "Action, Adventure", "2010", "Watched")];
+localStorage.setItem("libraryDB", JSON.stringify(testArray));
+
+function DisplayMovie( { movie }) {
     return (
         <div className="item">
             <img src={movie.posterLink} alt={movie.name} />
@@ -26,6 +30,8 @@ function displayMovie( { movie }) {
 
 export function Library() {
 
+    const [libraryDB, setLibraryDB] = useState(JSON.parse(localStorage.getItem("libraryDB")));
+
     return (
         <main>
 
@@ -37,46 +43,10 @@ export function Library() {
             <div className="library-content">
 
                 <div className="item-list">
-                    
-                    <div className="item">
-                        <img src="/placeholder_movie_poster.png" alt="Movie Poster"/>
-                        <div className="item-details">
-                            <h2>Movie Title</h2>
-                            <p>Genre: Action, Adventure<br/>
-                            Year: 2023<br/>
-                            Status: Watched</p>
-                        </div>
-                    </div>
 
-                    <div className="item">
-                        <img src="/placeholder_movie_poster.png" alt="Movie Poster"/>
-                        <div className="item-details">
-                            <h2>Another Movie</h2>
-                            <p>Genre: Comedy <br/>
-                            Year: 2022 <br/>
-                            Status: Watched</p>
-                        </div>
-                    </div>
-
-                    <div className="item">
-                        <img src="/placeholder_movie_poster.png" alt="Movie Poster"/>
-                        <div className="item-details">
-                            <h2>A Third Movie</h2>
-                            <p>Genre: Horror<br/>
-                            Year: 1987<br/>
-                            Status: To Watch</p>
-                        </div>
-                    </div>
-
-                    <div className="item">
-                        <img src="/placeholder_movie_poster.png" alt="Movie Poster"/>
-                        <div className="item-details">
-                            <h2>Movie #4</h2>
-                            <p>Genre: Action<br/>
-                            Year: 2026<br/>
-                            Status: To Watch</p>
-                        </div>
-                    </div>
+                    {[...libraryDB].reverse().map((movie, index) => {
+                        return <DisplayMovie key={index} movie={movie} />;
+                    })}
 
                 </div>
 
